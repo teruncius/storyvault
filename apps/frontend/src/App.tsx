d30abc4d78552f14a9a6a5c7c143e3d0fc29bd62
teruncius <teruncius@proton.me>
2025-11-23 12:17:11 +0100

    installed tanstack query

--- apps/frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@storyvault/frontend/components/ui/button"
-
-export function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
-}
