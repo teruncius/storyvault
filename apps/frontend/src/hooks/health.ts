1463ef9b0e8f23d38ef8780bf915eaef1df55b8b
teruncius <teruncius@proton.me>
2025-11-23 12:17:11 +0100

    installed prettier

--- apps/frontend/src/hooks/health.ts
+++ apps/frontend/src/hooks/health.ts
@@ -2,9 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 
 export function useHealth() {
     return useQuery({
-        queryKey: ['health'],
+        queryKey: ["health"],
         queryFn: async () => {
-            const response = await fetch('http://localhost:3000/health');
+            const response = await fetch("http://localhost:3000/health");
             return await response.json();
         },
     });
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@storyvault/frontend": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@storyvault/frontend": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+});
