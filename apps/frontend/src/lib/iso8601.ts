64dc1bee7709290831a5c51464f274163b01ff59
teruncius <teruncius@proton.me>
2025-11-28 11:54:09 +0100

    improved function signature

--- apps/frontend/src/lib/iso8601.ts
+++ apps/frontend/src/lib/iso8601.ts
@@ -1,8 +1,8 @@
-export function convertSecondsToISO8601(seconds: number) {
+export function convertSecondsToISO8601(seconds: number): string {
     return `PT${Math.abs(Math.floor(seconds))}S`;
 }
 
-export function convertISO8601ToSeconds(isoString: string) {
+export function convertISO8601ToSeconds(isoString: string): number {
     const match = isoString.match(/PT(\d+)S/);
     if (!match) {
         return 0;
