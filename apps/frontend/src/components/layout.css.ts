4e63fbf99f0fd6e06334568a00bbabd40b7f9311
teruncius <teruncius@proton.me>
2025-12-01 15:32:14 +0100

    removed layout shift on scrollbar

--- apps/frontend/src/components/layout.css.ts
+++ apps/frontend/src/components/layout.css.ts
@@ -6,6 +6,7 @@ export const container = style({
     flexDirection: "column",
     minHeight: "100vh",
     background: vars.color.backdrop,
+    scrollbarGutter: "stable",
 });
 
 export const header = style({
